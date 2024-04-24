d76bac6167c81710b50b2d0952210aa03f1f8a92
Ricardo Sacramento <sacramentor@webeo.com>
2024-04-24 16:35:07 +0100

    feat: removing snake case from enum

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,31 @@ use std::fmt::Display;
 
 use chrono::{prelude::*, Duration};
 
-enum support_dev {
+enum SupportDev {
     Ricardo,
     Dimitrios,
 }
 
-impl support_dev {
-    fn toggle(&self) -> support_dev {
+impl SupportDev {
+    fn toggle(&self) -> SupportDev {
         match self {
-            support_dev::Ricardo => support_dev::Dimitrios,
-            support_dev::Dimitrios => support_dev::Ricardo,
+            SupportDev::Ricardo => SupportDev::Dimitrios,
+            SupportDev::Dimitrios => SupportDev::Ricardo,
         }
     }
 }
 
-impl Display for support_dev {
+impl Display for SupportDev {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         return match self {
-            support_dev::Ricardo => write!(f, "Ricardo"),
-            support_dev::Dimitrios => write!(f, "Dimitrios"),
+            SupportDev::Ricardo => write!(f, "Ricardo"),
+            SupportDev::Dimitrios => write!(f, "Dimitrios"),
         };
     }
 }
 
 fn main() {
-    let mut support = support_dev::Dimitrios;
+    let mut support = SupportDev::Dimitrios;
     let now = Utc::now();
     let mut start_date = Utc.with_ymd_and_hms(2024, 04, 02, 0, 0, 0).unwrap();
 
